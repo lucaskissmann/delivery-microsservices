2d5f2b5c9144382b299947aab684aa851aa6a9ed
Lucas Kissmann <lucaskissmann@gmail.com>
2024-07-18 08:18:03 -0300

    feat: add pedidos microsservice

---
+++ pedidos/src/main/java/br/com/delivery/pedidos/controller/PedidoController.java
@@ -0,0 +1,64 @@
+package br.com.delivery.pedidos.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import br.com.delivery.pedidos.dto.PedidoDto;
+import br.com.delivery.pedidos.dto.StatusDto;
+import br.com.delivery.pedidos.service.PedidoService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/pedidos")
+public class PedidoController {
+
+    @Autowired
+    private PedidoService service;
+
+    @GetMapping()
+    public List<PedidoDto> listarTodos() {
+        return service.obterTodos();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PedidoDto> listarPorId(@PathVariable @NotNull Long id) {
+        PedidoDto dto = service.obterPorId(id);
+
+        return  ResponseEntity.ok(dto);
+    }
+
+    @GetMapping("/port")
+    public String retornaPorta(@Value("${local.server.port}") String port) {
+        return String.format("Requisição respondida pela instância executando na porta %s", port);
+    }
+
+    @PostMapping()
+    public ResponseEntity<PedidoDto> realizaPedido(@RequestBody @Valid PedidoDto dto, UriComponentsBuilder uriBuilder) {
+        PedidoDto pedidoRealizado = service.criarPedido(dto);
+
+        URI endereco = uriBuilder.path("/pedidos/{id}").buildAndExpand(pedidoRealizado.getId()).toUri();
+
+        return ResponseEntity.created(endereco).body(pedidoRealizado);
+
+    }
+
+    @PutMapping("/{id}/status")
+    public ResponseEntity<PedidoDto> atualizaStatus(@PathVariable Long id, @RequestBody StatusDto status){
+        PedidoDto dto = service.atualizaStatus(id, status);
+
+        return ResponseEntity.ok(dto);
+    }
+
+    @PutMapping("/{id}/pago")
+    public ResponseEntity<Void> aprovaPagamento(@PathVariable @NotNull Long id) {
+        service.aprovaPagamentoPedido(id);
+
+        return ResponseEntity.ok().build();
+    }
+}
